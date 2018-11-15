5fc344a8b5278cc6b222a1feb4936b1eea772549
Michael Scorsolini <admin@Michaels-MacBook-Pro.local>
2018-11-14 20:30:23 -0600

    Not Fully Implemented

---
+++ Food.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Food extends Artifact {
+	public Food(Scanner sc, int id)
+	{
+		super(sc, 8, id);
+	}
+	
+	@Override
+	public void use(Character c)
+	{
+		//check if health is full
+		//only give health up to capacity
+		
+		//consume food and recharge health
+		//remove from inventory
+		
+	}
+}
