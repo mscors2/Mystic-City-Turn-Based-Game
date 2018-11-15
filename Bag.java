b9f900ee29c441c9a9031adf851cab624788e0fe
Michael Scorsolini <admin@Michaels-MacBook-Pro.local>
2018-11-14 19:24:17 -0600

    Not Fully implemented

---
+++ Bag.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Bag extends Artifact {
+
+	public Bag(Scanner sc, int PlaceID)
+	{
+		super(sc, 5, PlaceID);
+	}
+	
+	@Override
+	public void use(Character c)
+	{
+		//increase capacity
+		//c.increaseCapacity()
+		//remove bag from inventory and game
+		
+	}
+}
