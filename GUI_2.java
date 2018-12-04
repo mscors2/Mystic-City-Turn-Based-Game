6eb3c3c85bffb97ea3f449ee3d5e390331c79741
vho6 <vho6@uic.edu>
2018-12-03 21:00:13 -0600

    vanson

--- GUI_2.java
+++ GUI_2.java
@@ -1,19 +1,22 @@
 /*
  * TODO: Vanson Ho
  */
-import javaxswing.*;
+import javax.swing.*;
 import java.util.*;
 
 public class GUI_2 implements UserInterface 
 {
 	private JFrame frame;
 
+	GUI_2()
+	{frame = null;}
+
 	public void display(String str)
 	{
 		if(frame != null)
 		{
-			setVisible(false);
-			dispose();
+			frame.setVisible(false);
+			frame.dispose();
 		}
 		frame = new JFrame("GUI_FRAME");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
