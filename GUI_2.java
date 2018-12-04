1ec5b78597e47f2775486f78ebc879bafe391905
vho6 <vho6@uic.edu>
2018-12-03 20:28:43 -0600

    Vansons gui

--- GUI_2.java
+++ GUI_2.java
@@ -1,18 +1,27 @@
 /*
  * TODO: Vanson Ho
  */
+import javax.swing.*;
+import java.util.*;
 
 public class GUI_2 implements UserInterface 
 {
 	public void display(String str)
 	{
-		// TODO
+		JFrame frame = new JFrame("GUI_FRAME");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel label = new JLabel(str);
+		frame.getContentPane().add(label);
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 	public String getLine()
 	{
-		// TODO
-		return "";
+		String input = JOptionPane.showInputDialog(null, "Input", "Please type a String", 0);
+		return input;
 	}
 	
 }
