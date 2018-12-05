08f43b8ccb33bdec3f28aa2e609f0d27d9877668
vho6 <32038887+vho6@users.noreply.github.com>
2018-12-05 13:40:45 -0600

    adding a bit

--- GUI_2.java
+++ GUI_2.java
@@ -2,36 +2,82 @@
  * TODO: Vanson Ho
  */
 import javax.swing.*;
+import java.awt.*;
 import java.util.*;
 
-public class GUI_2 implements UserInterface 
+public class GUI_2 implements UserInterface
 {
 	private JFrame frame;
+	private JTextArea textArea;
+	private JScrollPane scrollPane;
+	private JLabel label;
+	private JButton button;
+	/*
+        GUI_2()
+        {
+            frame = new JFrame("GUI_FRAME");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(600, 400);
+        }
 
+        public void display(String str)
+        {
+
+            frame = new JFrame("GUI_FRAME");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JLabel label = new JLabel(str);
+            frame.getContentPane().add(label);
+
+            frame.pack();
+            frame.setVisible(true);
+        }
+
+        */
 	GUI_2()
-	{frame = null;}
+	{
+
+		frame = new JFrame("MY_FRAME");
+		frame.setLayout(new FlowLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800,800);
+		frame.setTitle("The Best Game Ever");
+
+		/*
+		textArea = new JTextArea(5,30);
+		scrollPane = new JScrollPane(textArea);
+		button = new JButton("Enter this to end turn");
+		frame.setLayout(new FlowLayout());
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.add(scrollPane);
+
+*/
+
+		textArea = new JTextArea(20,60);
+
+		scrollPane = new JScrollPane(textArea);
+		frame.add(scrollPane);
+//		frame.add(textArea);
+		frame.setVisible(true);
+
+	}
 
 	public void display(String str)
 	{
-		if(frame != null)
-		{
-			frame.setVisible(false);
-			frame.dispose();
-		}
-		frame = new JFrame("GUI_FRAME");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JLabel label = new JLabel(str);
-		frame.getContentPane().add(label);
-
-		frame.pack();
+		textArea.setText(textArea.getText() + "\n" +  str);
+		textArea.update(textArea.getGraphics());
 		frame.setVisible(true);
+
 	}
-	
 	public String getLine()
 	{
 		String input = JOptionPane.showInputDialog(null, "Input", "Please type a String", 0);
+
+		textArea.setText(textArea.getText() + "\n" + "Making next move...");
 		return input;
 	}
-	
+
 }
